dc73d4ae507a6f06fd6eaf9bc72b043de01ddadd
howardlander <howard@renci.org>
2020-03-17 10:33:34 -0400

    updated docs to show that there are now 2 separate CloudTop docker at docker hub

---
+++ docs/source/Examples.rst
@@ -0,0 +1,6 @@
+Examples
+********
+.. image:: images/CloudTopArchitecture-Slide3.png
+   :align: center
+.. image:: images/CloudTopArchitecture-Slide4.png
+   :align: center
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'CloudTop'
-copyright = '2020, Howard Lander'
+copyright = '2020, The University of North Carolina at Chapel Hill'
 author = 'Howard Lander'
 
 # The full version, including alpha/beta/rc tags
@@ -48,6 +48,23 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+#    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
